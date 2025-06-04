9408e8f94ebc81a269432147fcb47d7826f7a078
Gabri <giacone101@gmail.com>
2025-06-04 21:13:34 +0200

    add ila initalize

--- examples_hw/apps/perf_test/vfpga_top.svh
+++ examples_hw/apps/perf_test/vfpga_top.svh
@@ -5,21 +5,21 @@ testLogic inst_test_Logic (
   .io_axi_ctrl_aw_ready(axi_ctrl.awready),
   .io_axi_ctrl_aw_payload_addr(axi_ctrl.awaddr),
   .io_axi_ctrl_aw_payload_prot(axi_ctrl.awprot),
-  .io_axi_ctrl_w_valid.(axi_ctrl.wvalid),
-  .io_axi_ctrl_w_ready.(axi_ctrl.wready),
+  .io_axi_ctrl_w_valid(axi_ctrl.wvalid),
+  .io_axi_ctrl_w_ready(axi_ctrl.wready),
   .io_axi_ctrl_w_payload_data(axi_ctrl.wdata),
   .io_axi_ctrl_w_payload_strb(axi_ctrl.wstrb),
   .io_axi_ctrl_b_valid(axi_ctrl.bvalid),
   .io_axi_ctrl_b_ready(axi_ctrl.bready),
   .io_axi_ctrl_b_payload_resp(axi_ctrl.bresp),
-  .io_axi_ctrl_ar_valid.(axi_ctrl.arvalid),
+  .io_axi_ctrl_ar_valid(axi_ctrl.arvalid),
   .io_axi_ctrl_ar_ready(axi_ctrl.arready),
   .io_axi_ctrl_ar_payload_addr(axi_ctrl.araddr),
   .io_axi_ctrl_ar_payload_prot(axi_ctrl.arprot),
   .io_axi_ctrl_r_valid(axi_ctrl.rvalid),
   .io_axi_ctrl_r_ready(axi_ctrl.rready),
   .io_axi_ctrl_r_payload_data(axi_ctrl.rdata),
-  .io_axi_ctrl_r_payload_resp(axi-ctrl.rresp),
+  .io_axi_ctrl_r_payload_resp(axi_ctrl.rresp),
   .io_sq_rd_valid(sq_rd.valid),
   .io_sq_rd_ready(sq_rd.ready),
   .io_sq_rd_payload_opcode(sq_rd.data.opcode),
@@ -86,6 +86,30 @@ testLogic inst_test_Logic (
   .io_axis_card_send_tlast(axis_card_send[0].tlast),
   .io_axis_card_send_tvalid(axis_card_send[0].tvalid),
   .io_axis_card_send_tready(axis_card_send[0].tready),
-  clk,
-  resetn
+  .clk(clk),
+  .resetn(resetn)
 );
+
+ila_0 inst_ila_test (
+  .clk(clk),
+  .probe0(axi_ctrl.awvalid),               // 1 bit
+  .probe1(axi_ctrl.awready),               // 1 bit
+  .probe2(axi_ctrl.awaddr),                // 8 bits
+  .probe3(axi_ctrl.wvalid),                // 1 bit
+  .probe4(axi_ctrl.wready),                // 1 bit
+  .probe5(axi_ctrl.wdata),                 // 64 bits
+  .probe6(sq_rd.valid),                    // 1 bit
+  .probe7(sq_rd.ready),                    // 1 bit
+  .probe8(sq_rd.data.vaddr),               // 48 bits
+  .probe9(sq_rd.data.len),                 // 2 bits
+  .probe10(sq_wr.valid),                   // 1 bit
+  .probe11(sq_wr.ready),                   // 1 bit
+  .probe12(sq_wr.data.vaddr),              // 48 bits
+  .probe13(sq_wr.data.len),                // 2 bits
+  .probe14(axis_card_recv[0].tdata),       // 160 bits
+  .probe15(axis_card_recv[0].tvalid),      // 1 bit
+  .probe16(axis_card_recv[0].tready),      // 1 bit
+  .probe17(axis_card_send[0].tdata),       // 160 bits
+  .probe18(axis_card_send[0].tvalid),      // 1 bit
+  .probe19(axis_card_send[0].tready)       // 1 bit
+)
